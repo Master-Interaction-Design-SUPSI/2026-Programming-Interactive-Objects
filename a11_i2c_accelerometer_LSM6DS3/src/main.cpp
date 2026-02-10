470cfa03ea0d89817f6418b2e9e63472dac8dfe3
Andreas Gysin <andreas@ertdfgcvb.ch>
2026-02-10 12:51:46 +0100

    Update main.cpp

--- a11_i2c_accelerometer_LSM6DS3/src/main.cpp
+++ a11_i2c_accelerometer_LSM6DS3/src/main.cpp
@@ -1,43 +1,54 @@
-	/**
-	 * Reading values from LSM6DS3
-	 * Connect the sensor as follows
-	 * VCC <-> 5V
-	 * GND <-> GND
-	 * SDA <-> 23
-	 * SCL <-> 2
-	 */
+/**
+ * Reading values from LSM6DS3
+ * Connect the sensor as follows
+ * VCC <-> 5V
+ * GND <-> GND
+ * SDA <-> 23
+ * SCL <-> 2
+ */
 
-	#include <Arduino.h>
-	#include <Wire.h>
-	#include "LSM6DS3.h"
-	LSM6DS3 imu(I2C_MODE, 0x6A);
+#include <Arduino.h>
+#include <Wire.h>
+#include "LSM6DS3.h"
+LSM6DS3 imu(I2C_MODE, 0x6A);
 
-	#define WIRE Wire
+#define WIRE Wire
+#define I2C_SDA 23
+#define I2C_SCL 2
 
-	#define I2C_SDA 23
-	#define I2C_SCL 2
+void setup() {
 
-	void setup() {
-	Serial.begin(115200);
-
+	Serial.begin(9600);
 
 	Wire.begin(I2C_SDA, I2C_SCL);
 
 	if (imu.begin() != 0) {
-		Serial.println("ERROR: LSM6DS3 initialisering feilet!");
 		while (1) {
-			delay(500);
+			Serial.println("ERROR initializing LSM6DS3.");
+			delay(1000);
 		}
 	}
 
-	Serial.println("LSM6DS3 initialisert!");
+	Serial.println("LSM6DS3 initialized.");
 
-	}
+}
 
 void loop() {
+
+	float gyroX = imu.readFloatGyroX();
+	float gyroY = imu.readFloatGyroY();
+	float gyroZ = imu.readFloatGyroZ();
+
 	float accelX = imu.readFloatAccelX();
 	float accelY = imu.readFloatAccelY();
 	float accelZ = imu.readFloatAccelZ();
+
+	Serial.print("gyroX = ");
+	Serial.println(gyroX);
+	Serial.print("gyroY = ");
+	Serial.println(gyroY);
+	Serial.print("gyroZ = ");
+	Serial.println(gyroZ);
 
 	Serial.print("accelX = ");
 	Serial.println(accelX);
